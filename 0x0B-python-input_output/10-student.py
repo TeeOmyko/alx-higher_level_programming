71944edf2770cb90229174c3c5af98c5607c7ecc
TeeOmyko <amanwachiteresa@gmail.com>
2023-04-12 09:59:28 -0700

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Defines a class Student."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_dict(self, attrs=None):
+        """Returns a dictionary representation of the Student instance.
+
+        If attrs is a list of strings, represents only those attributes included in the list.
+
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if attrs is not None and isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+        return self.__dict__
