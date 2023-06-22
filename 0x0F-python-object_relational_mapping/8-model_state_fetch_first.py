ffb1787ad6f2e71420ee754d83bc210411ba47cc
TeeOmyko <amanwachiteresa@gmail.com>
2023-06-22 01:08:35 -0700

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""cript that lists all State objects that contain the letter a from the
+"""script that lists all State objects that contain the letter a from the
 database hbtn_0e_6_usa
 """
 if __name__ == "__main__":
