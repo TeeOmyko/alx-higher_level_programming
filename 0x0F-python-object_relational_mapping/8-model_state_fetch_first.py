01126473d51c53ec7cd06cde4b76b7bf0004c2c2
TeeOmyko <amanwachiteresa@gmail.com>
2023-06-22 01:59:30 -0700

    changes

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-"""script that lists all State objects that contain the letter a from the
-database hbtn_0e_6_usa
 """
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 if __name__ == "__main__":
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    import sys
-    from model_state import Base, State
-    from sqlalchemy import (create_engine)
-    from sqlalchemy.orm import sessionmaker
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
-    session = Session()
-    Base.metadata.create_all(engine)
 
-    records = session.query(State).filter(
-        State.name.like('%a%')).order_by(State.id)
-    for record in records:
-        print("{}: {}".format(record.id, record.name))
-    session.close()
+    session = Session()
+    instance = session.query(State).order_by(State.id).first()
+
+    if instance is None:
+        print('Nothing')
+    else:
+        print('{0}: {1}'.format(instance.id, instance.name))
