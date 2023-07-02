872b674e30c6f109a0c13cbb9a348388f6445c80
TeeOmyko <amanwachiteresa@gmail.com>
2023-07-02 13:57:59 -0700

    body sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
-curl -sL "$1"#!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
-curl -sL "$1"
+# This program takes in a URL, sends a GET request to the URL, and displays the body of the response.
+curl -sL "$1" -X GET -D ./header -o ./output; if grep -q "200 OK" ./header; then cat ./output; fi
